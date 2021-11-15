22135567eadc09dac76b5fb348ad8142598fab9f
Rituraj Gupta <rituraj.iter@gmail.com>
2021-11-15 22:31:10 +0530

    Update and added

--- js/script.js
+++ js/script.js
@@ -2088,3 +2088,24 @@
 
     });
 }());
+
+var countDownDate = new Date("Dec 30, 2021 23:59:59").getTime();
+var myfunc = setInterval(function () {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    document.getElementById("day").innerHTML = days+"d"
+    document.getElementById("hours").innerHTML = hours+"h"
+    document.getElementById("mins").innerHTML = minutes+"m"
+    document.getElementById("secs").innerHTML = seconds+"s"
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("day").innerHTML = "O"
+        document.getElementById("hours").innerHTML = "V"
+        document.getElementById("mins").innerHTML = "E"
+        document.getElementById("secs").innerHTML = "R"
+    }
+}, 1000);
